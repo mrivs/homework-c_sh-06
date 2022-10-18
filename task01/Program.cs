3b8b7b2c1c5bd8a71c465e34eb7d18335cb0d189
mrivs <valentinilyins@gmail.com>
2022-10-18 23:01:55 +0300

    task 01; task 02 added

---
+++ task01/Program.cs
@@ -0,0 +1,35 @@
+﻿/*/
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+/*/
+
+double[] array = PrintStrAndGetArrayOfDouble("Введите числа (через пробел)");
+Console.WriteLine(PositiveNumbers(array));
+
+int PositiveNumbers(double[] array)
+{
+    int k = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) k++;
+    }
+    return k;
+}
+
+double[] PrintStrAndGetArrayOfDouble(string message) // пишем сообщение и задаем массив вещественных чисел
+{
+    Console.WriteLine(message);
+
+    string ArrayInOneString = (Console.ReadLine());
+    string[] ArrayOfString = ArrayInOneString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    double[] ArrayOfDouble = new double[ArrayOfString.Length];
+
+    for (int i = 0; i < ArrayOfDouble.Length; i++)
+    {
+        ArrayOfDouble[i] = double.Parse(ArrayOfString[i]);
+    }
+    return ArrayOfDouble;
+}
---
+++ task02/Program.cs
@@ -0,0 +1,28 @@
+﻿/*/
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+/*/
+
+double b1 = PrintAndGetValue("Введите b1");
+double k1 = PrintAndGetValue("Введите k1");
+double b2 = PrintAndGetValue("Введите b2");
+double k2 = PrintAndGetValue("Введите k2");
+
+double[]point=IntersectionPoint(b1,k1,b2,k2);
+Console.WriteLine("({0}; {1})",point[0],point[1]);
+
+double[] IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double[] point = new double[2];
+    point[0] = (b2 - b1) / (k1 - k2);
+    point[1] = k1 * point[0] + b1;
+    return point;
+}
+
+double PrintAndGetValue(string message) // пишем запрос, вводим число
+{
+    Console.WriteLine(message);
+    double d = double.Parse(Console.ReadLine());
+    return d;
+}
